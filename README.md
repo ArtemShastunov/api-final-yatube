fe10533587abaf36fe4832d08d40f2f7712bca54
Artem <aleshES2@yandex.ru>
2026-04-26 21:16:55 +1000

    fears

---
+++ README.md
@@ -0,0 +1,8 @@
+# API для Yatube
+REST API для социальной сети Yatube. Позволяет управлять публикациями, комментариями, подписками и просматривать тематические группы.
+
+## Установка
+1. Клонируйте репозиторий и перейдите в директорию проекта:
+```bash
+git clone https://github.com/ваш-username/api-final-yatube-main.git
+cd api-final-yatube-main
---
+++ yatube_api/api/serializers.py
@@ -0,0 +1,62 @@
+from rest_framework import serializers
+from rest_framework.relations import SlugRelatedField
+
+from posts.models import Comment, Follow, Group, Post
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class PostSerializer(serializers.ModelSerializer):
+    author = SlugRelatedField(slug_field='username', read_only=True)
+
+    class Meta:
+        fields = '__all__'
+        model = Post
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True, slug_field='username'
+    )
+    post = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    class Meta:
+        fields = '__all__'
+        model = Comment
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = '__all__'
+        model = Group
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True,
+        default=serializers.CurrentUserDefault()
+    )
+    following = serializers.SlugRelatedField(
+        slug_field='username',
+        queryset=User.objects.all()
+    )
+
+    class Meta:
+        model = Follow
+        fields = '__all__'
+        validators = [
+            serializers.UniqueTogetherValidator(
+                queryset=Follow.objects.all(),
+                fields=['user', 'following'],
+                message='Вы уже подписаны на этого пользователя'
+            )
+        ]
+
+    def validate_following(self, value):
+        if value == self.context['request'].user:
+            raise serializers.ValidationError(
+                'Нельзя подписаться на самого себя'
+            )
+        return value
